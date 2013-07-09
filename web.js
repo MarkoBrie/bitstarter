11d85e7a000d468996d934c8f2355220333130d8
Marko Briesemann <marko.briesemann@gmail.com>
2013-07-09 20:16:37 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var fs = require('fs');
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var text = fs.readFileSync('./index.html','utf8');
 
+    
     response.send(text);
-
+    console.log(text);
    response.send('Hello World2!');
 });
 
